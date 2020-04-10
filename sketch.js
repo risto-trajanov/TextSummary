a9cdd4e6a0b89d299fabc11f18aaded2cedffb53
Risto Trajanov <risto.trajanov@gmail.com>
2020-04-10 16:23:14 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+console.log("Pop up working");
+
+function setup() {
+    noCanvas();
+    let url = 'http://127.0.0.1:5000/main?url=';
+    var urlForSum = "";
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+        function (tabs) {
+            console.log(tabs[0].url);
+            urlForSum += tabs[0].url.toString();
+            url+=urlForSum;
+            loadJSON(url, gotData);
+        }
+    );
+    let p;
+
+    function gotData(data) {
+        p = createP(data.text);
+        p.parent('main');
+        console.log(data.text);
+    }
+
+
+}
